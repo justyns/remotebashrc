e170ca0281b4fd4ba9464d27109a2898559dad21
justyns <justyns@8c72df82-b379-0410-8030-b2a273824298>
2009-12-03 04:58:37 +0000

    - changed the default PS1 prompt. - added /sbin and /usr/sbin to PATH - added a $debug option to remotebashrc.sh
    git-svn-id: https://justynshull.com/dev/remotebashrc@8 8c72df82-b379-0410-8030-b2a273824298

--- remotebashrc.sh
+++ remotebashrc.sh
@@ -28,7 +28,8 @@
 # random_file doesn't always work.   $RANDOM returns nothing some times. 
 # check to see if it's a freebsd box before trying to ssh in..  /bin/bash doesn't seem to be installed by default
 [ $# = 0 ] && { sed -n -e '/^# Usage:/,/^$/ s/^# \?//p' < $0; exit; }
-beclean=1 #1 = don't automatically source the bashrc-remote, but still upload it.  0 = upload and use --rcfile to source it(looks bad in top/ps)
+beclean=0 #1 = don't automatically source the bashrc-remote, but still upload it.  0 = upload and use --rcfile to source it(looks bad in top/ps)
+debug=1
 bashrc_remote=~/bashrc-remote
 random_file=/tmp/rshrc-`date +%d%m%g%H%M%S`.$RANDOM 
 #random_file=`mktemp -t remoteshrc`;
@@ -36,15 +37,21 @@ ruser=$1
 rhost=$3
 rport=$2
 
+f_debug() { [ $debug ] && echo "$1"; }
 green="\033[01;32m";
+f_debug "initiating master connection.."
 ssh -NfnMS ~/.ssh/master-%r@%h:%p $ruser@$rhost -p $rport
+f_debug "uploading $random_file to remote server..."
 scp -o ControlPath=~/.ssh/master-%r@%h:%p -P $rport $bashrc_remote $ruser@$rhost:$random_file
 #ssh -S ~/.ssh/master-%r@%h:%p -l root $ruser bash --login -c 'source /tmp/bashrc-remotejs13;rm -f /tmp/bashrc-remotejs13'
-if [ $beclean ]; then
+if [ $beclean -eq "1" ]; then
+	f_debug "beclean set to 1, not using --rcfile"
 	echo -e "$green once logged in, run: source $random_file \e[m"
 	ssh -t -S ~/.ssh/master-%r@%h:%p $ruser@$rhost -p $rport
 else
+	f_debug "beclean set to 0, using --rcfile to have bashrc-remote automatically sourced"
 	ssh -t -S ~/.ssh/master-%r@%h:%p $ruser@$rhost -p $rport "bash --rcfile $random_file;rm -vf $random_file"
 fi
 #ssh -S ~/.ssh/master-%r@%h:%p -l root $1 "rm -vf /tmp/bashrc-remotejs13"
+f_debug "shell's closed..  sending exit signal to master connection"
 ssh -vO exit -S ~/.ssh/master-%r@%h:%p $ruser@$rhost -p $rport
